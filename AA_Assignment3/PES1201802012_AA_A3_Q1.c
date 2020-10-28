35179126969b966b5b35307767dbc554eaeac3dd
Sreyans Bothra <sreyansrocks@gmail.com>
2020-10-28 12:11:17 +0530

    3D DP and LCS and edit distance

---
+++ AA_Assignment3/PES1201802012_AA_A3_Q1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int mod=1000000007;
+
+int main()
+{
+    long long int dpt[201][67][4];
+    for(int row=0;row<201;++row)
+        for(int k=0;k<67;++k)
+            for(int clue=0;clue<4;++clue)
+                dpt[row][k][clue]=0;
+    //base cases
+    dpt[1][0][0]=1;dpt[1][0][3]=25;
+
+    //filling the dp table
+    for(int n=2;n<201;++n)
+        {
+            dpt[n][0][0]=(dpt[n-1][0][0]+dpt[n-1][0][1]+dpt[n-1][0][3])%mod;
+            dpt[n][0][1]=dpt[n-1][0][0];
+            dpt[n][0][2]=dpt[n-1][0][1];
+            dpt[n][0][3]=((((dpt[n-1][0][3]+dpt[n-1][0][2])%mod)*25)%mod+(((dpt[n-1][0][0]+dpt[n-1][0][1])%mod)*24)%mod)%mod;
+            for(int k=1;3*k+1<=n;++k)
+            {
+                dpt[n][k][0]=(dpt[n-1][k-1][2]%mod+(dpt[n-1][k][0]+dpt[n-1][k][1]+dpt[n-1][k][3])%mod)%mod;
+                dpt[n][k][1]=dpt[n-1][k][0];
+                dpt[n][k][2]=dpt[n-1][k][1];
+                dpt[n][k][3]=((((dpt[n-1][k][3]+dpt[n-1][k][2])%mod)*25)%mod+(((dpt[n-1][k][0]+dpt[n-1][k][1])%mod)*24)%mod)%mod;
+            }
+        }
+    //printf("%lld %lld %lld %lld %lld %lld %lld \n",dpt[4][0][0],dpt[4][0][1],dpt[4][0][2],dpt[4][0][3],dpt[4][1][0],dpt[4][1][1],dpt[3][0][2]);
+        
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;int k;
+        scanf("%d %d",&n,&k);
+        if (3*k+1>n)
+            printf("0\n");
+        else
+            printf("%lld\n",(dpt[n][k][0]+dpt[n][k][1]+dpt[n][k][2]+dpt[n][k][3])%mod);
+    }
+}
---
+++ AA_Assignment3/PES1201802012_AA_A3_Q3.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int mod=1000000007;
+
+void convert(char* s,int lenofs)
+{
+    //int lenofs=strlen(s);
+    for(int i=0;i<lenofs;++i)
+    {
+        if(s[i]>64 && s[i]<91)
+            s[i]+=32;
+    }
+}
+
+int minfind(int a,int b,int c)
+{
+    return a<b?(a<c?a:c):(b<c?b:c);
+}
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        char* sm=(char*)malloc(1005);
+        char* sn=(char*)malloc(1005);
+        scanf("%s %s",sm,sn);
+
+        int m=strlen(sm);
+        int n=strlen(sn);
+        convert(sm,m);
+        convert(sn,n);
+        
+        //has m+1 rows and n+1 columns, therefore to access lpstable[x][y] = lpstable[(x)*(n+1)+y]
+        int* lpstable=(int*)malloc((m+1)*(n+1)*sizeof(int));
+        for(int i=0;i<=m;++i)
+        {
+            for(int j=0;j<=n;++j)
+            {
+                if (i==0 || j==0)
+                    lpstable[i*(n+1)+j]=j==0?i:j;
+                else
+                    {
+                        if (sm[i-1]==sn[j-1])
+                            lpstable[i*(n+1)+j]=(lpstable[(i-1)*(n+1)+j-1]);
+                        else
+                            lpstable[i*(n+1)+j]=(1+minfind(lpstable[(i-1)*(n+1)+j-1],lpstable[(i-1)*(n+1)+j],lpstable[(i)*(n+1)+j-1]))%mod;
+                    }             
+            }
+        }
+        printf("%d\n",(lpstable[m*(n+1)+n])%mod);
+	free(lpstable);
+        free(sm);
+        free(sn);
+    }
+}
+/*
+test case:
+3
+ABC bD
+space ship
+ship dock
+*/
